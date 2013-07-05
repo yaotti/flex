404f8557ae2c955d31c24652d79dc785850c1e04
Domizio Demichelis <dd.nexus@gmail.com>
2013-07-05 22:05:59 +0200

    migration > live_reindex refactoring

--- lib/flex/utility_methods.rb
+++ lib/flex/utility_methods.rb
@@ -48,7 +48,7 @@ module Flex
       lines = args[:collection].map do |d|
                 m = {}
                 index = get_index(d) || args[:index]  || Conf.variables[:index]
-                index = ModelMigration.prefix_index(index) if args[:model_migration]
+                index = ModelLiveReindex.prefix_index(index) if args[:live_reindex]
                 m['_index']   = index
                 m['_type']    = get_type(d)  || args[:type] || Conf.variables[:type]
                 m['_id']      = get_id(d)    || d       # we could pass an array of ids to delete
